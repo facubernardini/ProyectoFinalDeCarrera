9df6faab1ff41b85db8e18d36a1dd156570b576f
Facundo Bernardini <facubernardini@hotmail.com>
2024-11-14 00:06:40 -0300

    Primeras pruebas con script mesh cutter. Agregue modelos low poly para que las mallas se corten mas rapido

--- FrameworkCristalografia/Assets/MeshSlicer/Demo/Script/ClickToCut.cs
+++ FrameworkCristalografia/Assets/MeshSlicer/Demo/Script/ClickToCut.cs
@@ -6,17 +6,24 @@ namespace com.marufhow.meshslicer.demo
     public class ClickToCut : MonoBehaviour
     {
         public MHCutter _mhCutter;
-        public GameObject planoCorte, cubo;
+        public GameObject planoCorte;
+        public GameObject[] celdas;
         private Vector3 posicionCorte, direccionCorte;
 
         private void Update()
         {
             posicionCorte = planoCorte.transform.position;
             direccionCorte = planoCorte.transform.up;
+        }
 
-            if (Input.GetMouseButtonDown(0))
+        public void CortarMallas()
+        {
+            for (int i = 0; i < celdas.Length; i++)
             {
-                _mhCutter.Cut(cubo, posicionCorte, direccionCorte);
+                if (celdas[i].activeSelf)
+                {
+                    _mhCutter.Cut(celdas[i], posicionCorte, direccionCorte);
+                }
             }
         }
     }
